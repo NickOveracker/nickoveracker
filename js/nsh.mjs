4a289d3cc848c077c0c78ee3a7d8a573c74d8dd2
Nick Overacker <nick.overacker@ymail.com>
2025-09-02 07:38:52 +0900

    Caret highlighting. No more cool breathing animation, but sacrifices must be made.

--- js/nsh.mjs
+++ js/nsh.mjs
@@ -23,6 +23,9 @@ const nostream = {
     println: (_) => {},
 }
 
+let history = [];
+let historyIdx = -1;
+
 function resolveCommand(inputs, ctx) {
     let file = getFile(inputs[0], ctx);
 
@@ -54,6 +57,7 @@ function execute() {
     const inputDiv  = document.createElement("div");
     const inputText = active_prompt.innerText;
     const inputTextNode = document.createTextNode(`${inputText}`);
+    history.unshift(inputText);
     inputDiv.classList.add("prompt");
     inputDiv.appendChild(inputTextNode);
     active_prompt.innerText = "";
@@ -97,6 +101,23 @@ window.addEventListener("keydown", (e) => {
       e.preventDefault();    // block newline insertion
       e.stopPropagation();   // stop it from bubbling further
       execute();
+    } else if (e.key === 'ArrowUp') {
+        if(historyIdx + 1 < history.length) {
+            historyIdx++;
+            active_prompt.innerText = history[historyIdx];
+            active_prompt.selectionStart = active_prompt.selectionEnd;
+        }
+    } else if (e.key === 'ArrowDown') {
+        if (historyIdx >= 0) {
+            historyIdx--;
+            active_prompt.innerText = historyIdx < 0 ? "" : history[historyIdx];
+            setTimeout(function(){ this.selectionStart = this.selectionEnd = 10000; }.bind(active_prompt), 0);
+        }
+    } else if (e.key === 'Escape') {
+        if (historyIdx >= 0) {
+            historyIdx = -1;
+            active_prompt.innerText = "";
+        }
     } else if (document.activeElement !== active_prompt) {
       active_prompt.focus();
     }
@@ -106,3 +127,14 @@ active_prompt_container.addEventListener("click", (e) => {
     active_prompt.focus();
 });
 
+active_prompt.oninput = () => {
+    shadow.innerHTML = active_prompt.innerText + "&nbsp;";
+}
+
+document.onselectionchange = () => {
+    const offset = document.getSelection().anchorOffset;
+    const before = shadow.innerText.substring(0, offset);
+    const after  = shadow.innerText.substring(offset + 1);
+    const middle = shadow.innerText.charAt(offset);
+    shadow.innerHTML = `${before}<span class="caret">${middle}</span>${after}`;
+}
